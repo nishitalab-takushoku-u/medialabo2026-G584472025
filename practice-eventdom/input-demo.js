a5d5da9586749fd572957413eae1c5f2968bf2ee
Sato Nagisa <258447@st.takushoku-u.ac.jp>
2026-05-14 15:38:48 +0900

    a

--- practice-eventdom/input-demo.js
+++ practice-eventdom/input-demo.js
@@ -2,4 +2,11 @@ let b = document.querySelector('button#print');
 b.addEventListener('click', greeting);
 function greeting() {
     console.log('こんにちは');
+    let i = document.querySelector('input[name="shimei"]');
+    i.getAttribute('name');
+    i.value;
+    let p = document.querySelector;
+    p = document.querySelector('p#message');
+    let aisatsu = 'こんにちは, ' + i.value + ' さん';
+    p.textContent = aisatsu;
 }

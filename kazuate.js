664014f3932558f38419b318d498a03eeb963777
Sato Nagisa <258447@st.takushoku-u.ac.jp>
2026-05-14 17:27:19 +0900

    a

--- kazuate.js
+++ kazuate.js
@@ -12,19 +12,34 @@ let kaisu = 0;
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // ここから: 予想回数を1増やして，span#kaisu 要素のテキストを更新
-
+  kaisu = kaisu + 1;
+  let p = document.querySelector('span#kaisu');
+  p.textContent = kaisu;
   // ここまで: 予想回数を1増やして，span#kaisu 要素のテキストを更新
   
   // ここから: テキストボックスに指定された数値を yoso に代入する
-  let yoso;
+  let a = document.querySelector('input[name="yoso"]');
+  let yoso = Number(a);
   // ここまで: テキストボックスに指定された数値を yoso に代入する
   
   // ここから: 正解判定する
   // 　　　　  正解/不正解のときのメッセージを表示する
+  for (let i=0; i<3; i=i+1) {
+    if (kotae === yoso) {
+      console.log('正解です!');
+    }
+    else if (kotae > yoso) {
+      console.log('不正解です. 答えは ' + yoso + ' より大きいです.');
+    }
+    else {
+      console.log('不正解です. 答えは ' + yoso + ' より小さいです.');
+    }
+  }
 
   // ここまで: 正解判定する
 }
 
 // ここから: ボタンを押した時のイベントハンドラとして hantei を登録
-
+let b = document.querySelector('button#hantei');
+b.addEventListener('click', hantei);
 // ここまで: ボタンを押した時のイベントハンドラとして hantei を登録
